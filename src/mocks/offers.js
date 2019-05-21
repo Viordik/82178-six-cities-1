6cac530ccc77c44111995dd52bfbeeaa07f30c9b
Viordik <fataliev2009@rambler.ru>
2019-05-22 00:00:02 +0300

    Добавлены новые данные с объектами\ по городам

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,4 +1,5 @@
 export default [
+  // Paris
   {
     id: 1,
     premium: true,
@@ -7,45 +8,331 @@ export default [
     bookmarks: false,
     title: `Beautiful & luxurious apartment at great location`,
     type: `Apartment`,
-    coordinates: [52.3909553943508, 4.85309666406198]
+    city: `Paris`,
+    location: [48.875076, 2.315605]
   },
   {
     id: 2,
+    premium: false,
+    srcImg: `img/apartment-01.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Paris`,
+    location: [48.865076, 2.305605]
+  },
+  {
+    id: 3,
+    premium: false,
+    srcImg: `img/apartment-01.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    city: `Paris`,
+    location: [48.873955, 2.324347]
+  },
+  {
+    id: 4,
+    premium: true,
+    srcImg: `img/apartment-01.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    city: `Paris`,
+    location: [48.863955, 2.314347]
+  },
+  {
+    id: 5,
+    premium: false,
+    srcImg: `img/apartment-01.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Paris`,
+    location: [48.865899, 2.325992]
+  },
+  // Cologne
+  {
+    id: 6,
+    premium: true,
+    srcImg: `img/room.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    city: `Cologne`,
+    location: [50.930544, 6.937651]
+  },
+  {
+    id: 7,
     premium: false,
     srcImg: `img/room.jpg`,
     price: 80,
     bookmarks: false,
     title: `Wood and stone place`,
     type: `Private room`,
-    coordinates: [52.369553943508, 4.85309666406198]
+    city: `Cologne`,
+    location: [50.920544, 6.927651]
   },
   {
-    id: 3,
+    id: 8,
+    premium: false,
+    srcImg: `img/room.jpg`,
+    price: 132,
+    bookmarks: false,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    city: `Cologne`,
+    location: [50.910544, 6.917651]
+  },
+  {
+    id: 9,
+    premium: true,
+    srcImg: `img/room.jpg`,
+    price: 180,
+    bookmarks: false,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    city: `Cologne`,
+    location: [50.900544, 6.907651]
+  },
+  {
+    id: 10,
+    premium: false,
+    srcImg: `img/room.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Cologne`,
+    location: [50.830544, 6.937651]
+  },
+  // Brussels
+  {
+    id: 11,
+    premium: true,
+    srcImg: `img/apartment-02.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    city: `Brussels`,
+    location: [50.854283, 4.352131]
+  },
+  {
+    id: 12,
+    premium: false,
+    srcImg: `img/apartment-02.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Brussels`,
+    location: [50.844283, 4.352131]
+  },
+  {
+    id: 13,
     premium: false,
     srcImg: `img/apartment-02.jpg`,
     price: 132,
     bookmarks: false,
     title: `Canal View Prinsengracht`,
     type: `Apartment`,
-    coordinates: [52.3909553943508, 4.929309666406198]
+    city: `Brussels`,
+    location: [50.854283, 4.342131]
   },
   {
-    id: 4,
+    id: 14,
+    premium: true,
+    srcImg: `img/apartment-02.jpg`,
+    price: 180,
+    bookmarks: false,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    city: `Brussels`,
+    location: [50.834283, 4.332131]
+  },
+  {
+    id: 15,
+    premium: false,
+    srcImg: `img/apartment-02.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Brussels`,
+    location: [50.824283, 4.352131]
+  },
+  // Amsterdam
+  {
+    id: 16,
+    premium: true,
+    srcImg: `img/apartment-03.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    city: `Amsterdam`,
+    location: [52.3909553943508, 4.85309666406198]
+  },
+  {
+    id: 17,
+    premium: false,
+    srcImg: `img/apartment-03.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Amsterdam`,
+    location: [52.369553943508, 4.85309666406198]
+  },
+  {
+    id: 18,
+    premium: false,
+    srcImg: `img/apartment-03.jpg`,
+    price: 132,
+    bookmarks: false,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    city: `Amsterdam`,
+    location: [52.3909553943508, 4.929309666406198]
+  },
+  {
+    id: 19,
     premium: true,
     srcImg: `img/apartment-03.jpg`,
     price: 180,
     bookmarks: false,
     title: `Nice, cozy, warm big bed apartment`,
     type: `Apartment`,
-    coordinates: [52.3809553943508, 4.939309666406198]
+    city: `Amsterdam`,
+    location: [52.3809553943508, 4.939309666406198]
   },
   {
-    id: 5,
+    id: 20,
+    premium: false,
+    srcImg: `img/apartment-03.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Amsterdam`,
+    location: [52.3809553943508, 4.919309666406198]
+  },
+  // Hamburg
+  {
+    id: 21,
+    premium: true,
+    srcImg: `img/room.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    city: `Hamburg`,
+    location: [53.552645, 9.966287]
+  },
+  {
+    id: 22,
     premium: false,
     srcImg: `img/room.jpg`,
     price: 80,
     bookmarks: false,
     title: `Wood and stone place`,
-    type: `Private room`
+    type: `Private room`,
+    city: `Hamburg`,
+    location: [53.542645, 9.966287]
+  },
+  {
+    id: 23,
+    premium: false,
+    srcImg: `img/room.jpg`,
+    price: 132,
+    bookmarks: false,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    city: `Hamburg`,
+    location: [53.592645, 9.966287]
+  },
+  {
+    id: 24,
+    premium: true,
+    srcImg: `img/apartment-03.jpg`,
+    price: 180,
+    bookmarks: false,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    city: `Hamburg`,
+    location: [53.582645, 9.966287]
+  },
+  {
+    id: 25,
+    premium: false,
+    srcImg: `img/room.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Hamburg`,
+    location: [53.572645, 9.986287]
+  },
+  // Dusseldorf
+  {
+    id: 26,
+    premium: true,
+    srcImg: `img/apartment-01.jpg`,
+    price: 120,
+    bookmarks: false,
+    title: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    city: `Dusseldorf`,
+    location: [51.230569, 6.787428]
+  },
+  {
+    id: 27,
+    premium: false,
+    srcImg: `img/room.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Dusseldorf`,
+    location: [51.240569, 6.787428]
+  },
+  {
+    id: 28,
+    premium: false,
+    srcImg: `img/apartment-02.jpg`,
+    price: 132,
+    bookmarks: false,
+    title: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    city: `Dusseldorf`,
+    location: [51.220569, 6.787428]
+  },
+  {
+    id: 29,
+    premium: true,
+    srcImg: `img/apartment-03.jpg`,
+    price: 180,
+    bookmarks: false,
+    title: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    city: `Dusseldorf`,
+    location: [51.250569, 6.787428]
+  },
+  {
+    id: 25,
+    premium: false,
+    srcImg: `img/room.jpg`,
+    price: 80,
+    bookmarks: false,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    city: `Dusseldorf`,
+    location: [51.230569, 6.777428]
   }
 ];
